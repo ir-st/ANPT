4a463ca544f08e893017266bf61fc1648289da99
Cluren <cluren@foxmail.com>
2020-06-05 22:43:36 +0800

    main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+print('hello world!')
 
-os.system("./pynmap")
-os.system("./metasploit")
+os.system("python3 ./nmap/pynmap.py")
+# os.system("python3 nesses.py")
+# os.system("python3 baopo.py")
+os.system("python3 ./vulnexp/metasploit.py")
