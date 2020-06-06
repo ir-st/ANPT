d90dd1fef6d72182aceb630b01c84e3ee86bc939
adomua <60392368+adomua@users.noreply.github.com>
2020-06-06 19:55:38 +0800

    Update shangchuan.py

--- shangchuan.py
+++ shangchuan.py
@@ -7,17 +7,17 @@ conn = MongoClient('127.0.0.1', 27017)
 # 连接 test 数据库bai，没有则自动创建
 db = conn.test  
  
-# 使用du students 集合，没有则自动创建
+# 使用du results 集合，没有则自动创建
 results = db.results
  
-# 打开学生信息文件, 并将数据存入到数据库
+# 打开扫描信息文件, 并将数据存入到数据库
 with open(r'C:\Users\dell\Desktop\result.txt', 'r') as f:
      for line in f.readlines():
-    # 分割学生信息
+    # 分割信息
      	items = line.strip('\r').strip('\n').split(',')    
     # 添加到数据库
      	students.insert({ 'stu_id': items[0], 'name': items[1], 'grade': int(items[2]) })
  
-# 数据库查询学生信息并打印出来
+# 数据库查询信息并打印出来
 for s in results.find():
     print(s)
