74f84ec001309fbd01797008375b239883567988
A2ur3 <iridium-satellite@outlook.com>
2020-06-10 21:39:15 +0800

    scanner payloads

---
+++ payloads/directory_browse.py
@@ -0,0 +1,28 @@
+
+from lib.core.enums import VulType, PLACE
+from lib.core.plugins import PluginBase
+
+
+class W13SCAN(PluginBase):
+    name = '目录遍历插件'
+    desc = '''遍历每个目录，查看是否可以直接访问'''
+
+    def audit(self):
+
+        resp_str = self.response.text
+        flag_list = [
+            "directory listing for",
+            "<title>directory",
+            "<head><title>index of",
+            '<table summary="directory listing"',
+            'last modified</a>',
+
+        ]
+        for i in flag_list:
+            if i in resp_str.lower():
+                result = self.new_result()
+                result.init_info(self.requests.url, "目录遍历", VulType.SENSITIVE)
+                result.add_detail("payload请求", self.requests.raw, self.response.raw,
+                                  "匹配到关键词:{}".format(i), "", "", PLACE.GET)
+                self.success(result)
+                break
